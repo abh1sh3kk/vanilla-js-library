9932febb754fe80992458c2e68d7af5078e11081
Abhishek Acharya <abhishekacharya171@gmail.com>
2022-03-25 22:38:29 +0545

    Can add cards now and form data is accessed in js

--- script.js
+++ script.js
@@ -1,53 +1,128 @@
-const headline = document.querySelector('.headline');
 const plus = document.querySelector(".addBook"); 
+
 const form = document.querySelector(".formContainer");
 const cancelButton = document.querySelector(".cancel");
+const theForm = document.querySelector("form");
+let submitButton = document.querySelector(".submit");
+let bookName = document.querySelector(".bookName");
+let bookAuthor = document.querySelector(".bookAuthor");
+let totalPages = document.querySelector(".totalPages");
+let completedPages = document.querySelector(".pagesCompleted");
+let displayBookName = document.querySelector(".book-name");
+let card__readPages = document.querySelector(".read-pages");
+let card__totalPages = document.querySelector(".total-pages");
 
-headline.addEventListener("mouseover", function() {
-	this.setAttribute("style","color: cyan");
-})
-headline.addEventListener("mouseout", function() {
-	this.setAttribute("style", "color: white");
-})
+let formData = new FormData(form);
 
 
-// ****************************************************
+let cardContainer = document.querySelector(".card-container");
+
+// ***************************************************
 
 let bookList = []; 
 
 let book = {
-	name: "science", 
-	pages: 300
+	name: "science",
+	author: "koi na koi",
+	tPages: "212", 
+	cPages: 300
 };
 cancelButton.addEventListener("click", function() {
 	form.setAttribute("style", "visibility: hidden");
 })
 
 plus.addEventListener("click", function() {
-
 	form.setAttribute("style", "visibility: visible");
+});
+submitButton.addEventListener("click", () => {
 	let book1 = Object.create(book);
-
-	let randomPage = Math.floor(Math.random() * 500);
-
-	// book1.name = prompt("Enter the name of the book: ");
-	// book1.pages = prompt("Enter the no. of pages:");
-
-	book1.name = `Book No. ${randomPage}`;
-	book1.pages = randomPage;
-
+	
+	book1.name = bookName.value;
+	book1.author = bookAuthor.value;
+	book1.tPages = totalPages.value; 
+	book1.cPages = completedPages.value; 
+	
 	bookList.push(book1);
-
+	
 	console.clear();
+	console.log(book1);
+	
+	displayBookName.value = book1.name;
+	
+	console.log("submitted");
+	cardGenerator("How to make library in js", "- Abhishek Acharya", "6969", "69");
 
-
-	for (let i=0; i < bookList.length; i++) { 
-		console.log(bookList[i]);
-	}
-
-	console.log("Information: ");
-	console.log(`Number of books: ${bookList.length}`);
+	form.setAttribute("style", "visibility: hidden");
 });
 
+function cardGenerator(book, author, tPages, cPages) {
 
+	// create a card
+	let newCard = document.createElement("div");
+	cardContainer.insertBefore(newCard,plus);
+	newCard.classList.add("card");
+
+	// create content part 
+	let newCardContent = document.createElement("div");
+	newCard.appendChild(newCardContent);
+	newCardContent.classList.add("card-content");
+
+	// create book and author name section
+	let newCardBookName = document.createElement("div");
+	let newCardAuthorName = document.createElement("div");
+
+	// adding class to book and author
+	newCardBookName.classList.add("book-name");
+	newCardAuthorName.classList.add("author-name");
+
+	// appending them inside card 
+	newCardContent.appendChild(newCardBookName);
+	newCardContent.appendChild(newCardAuthorName);
+
+	// to update their value
+	newCardBookName.textContent = book;
+	newCardAuthorName.textContent = author;
+
+	// create footer
+	let newCardFooter = document.createElement("div");
+	newCard.appendChild(newCardFooter);
+	newCardFooter.classList.add("card-footer");
+
+	// creating total and completed pages
+	let newCardTotalPages = document.createElement("div");
+	let newCardCompletedPages = document.createElement("div");
+
+	// appending them inside footer
+	newCardFooter.appendChild(newCardCompletedPages);
+	newCardFooter.appendChild(newCardTotalPages);
+
+	// adding class
+	newCardTotalPages.classList.add("total-pages");
+	newCardCompletedPages.classList.add("read-pages");
+
+	// adding content
+	newCardTotalPages.textContent = tPages;
+	newCardCompletedPages.textContent = cPages;
+
+
+	
+
+
+	
+
+
+
+
+
+}
+
+
+
+
+
+	
+// let randomPage = Math.floor(Math.random() * 500);
+//Assigning the input in object
+// book1.name = `Book No. ${randomPage}`;
+// book1.pages = randomPage;
 
