6742d50a0409811c349a37d0f53bffe81c5b01eb
Abhishek Acharya <abhishekacharya171@gmail.com>
2022-07-03 20:21:08 +0545

    Major refactor of the history

--- script.js
+++ script.js
@@ -21,6 +21,38 @@ let info__remainingBooks = document.querySelector(".remainingBooks");
 let formData = new FormData(form);
 let cardContainer = document.querySelector(".card-container");
 let bookList = {};
+let readyMadeList = [{
+		"name": "Organize your whiteboard", 
+		"author": "Narendra Bohora", 
+		"tPages": 99,
+		"cPages": 22
+	}, 
+	{
+		"name": "How To Control Your Anger", 
+		"author": "Balkrishna Subedi", 
+		"tPages": 127,
+		"cPages": 38
+	},
+	{
+		"name": "Control Your Students: The Science Of Silent Class", 
+		"author": "Navaraj Negi", 
+		"tPages": 118,
+		"cPages": 42
+	},
+	{
+		"name": "How to be productive teacher and skip the easy topics", 
+		"author": "Yogesh Raj Subedi", 
+		"tPages": 135,
+		"cPages": 46
+	},
+	{
+		"name": "The Subtle Art Of Acting", 
+		"author": "Anmol KC", 
+		"tPages": 69,
+		"cPages": 41
+	},
+	
+]
 
 // **************** DRIVER CODES ********************************
 renderCards();
@@ -45,7 +77,6 @@ function book(name, author, tPages, cPages) {
 function renderCards() {
 	bookList = JSON.parse(localStorage.getItem("bookList"));
 	clearCards();
-
 	generateAllCards();
 	updateInfo();
 }
@@ -54,6 +85,7 @@ function clearCards() {
 		cardContainer.removeChild(cardContainer.firstElementChild);
 	}
 }
+
 function generateAllCards() {
 	for(let i=0; i<bookList.length; i++) {
 		cardGenerator(bookList[i].name, bookList[i].author, bookList[i].tPages, bookList[i].cPages);
@@ -132,98 +164,80 @@ function cardGenerator(book, author, tPages, cPages) {
 function updateInfo() {
 	info__numberOfBooks.textContent = bookList.length;
 }	
+const showForm = () => {
+	form.style.visibility = "visible";
+};
+const hideForm = () => { 
+	form.style.visibility = "hidden";
+	hideErrorMessage();
+}
+const removeAllBooks = () => {
+	bookList.length = 0;
+	localStorage.setItem("bookList", JSON.stringify(bookList));
+}
+const clearAndRender = () => {
+	removeAllBooks();
+	renderCards();
+}
+const showErrorMessage = () => {
+	errorMessage_with_X.style.visibility = "visible"; 
+}
+const hideErrorMessage = () => {
+	errorMessage_with_X.style.display = "none"; 
+}
+const isFormValid = () => {
+	let isValid = true;
 
 
+
+
+	if (bookAuthor.value.length > 30) showErrorMessage();
+	if (+completedPages.value > +totalPages.value) {
+		isValid = false;
+		showErrorMessage();
+	}
+
+
+
+
+
+	return isValid;
+}
+const updateLocalStorage = () => {
+	localStorage.setItem("bookList", JSON.stringify(bookList));
+}
+const updateDatabase = () => {
+	// creating book object
+	let bookObj = book(
+		bookName.value, 
+		bookAuthor.value, 
+		totalPages.value, 
+		completedPages.value); 	
+
+	bookList.push(bookObj);
+
+	updateLocalStorage();	
+}
+const handleForm = (e) => {
+	e.preventDefault();
+	
+	if (isFormValid()) { 
+		hideForm();
+		updateDatabase();
+		renderCards();
+		updateInfo();
+	}	
+}
+const populateLibrary = () => {
+		bookList = readyMadeList;
+		localStorage.setItem("bookList", JSON.stringify(bookList));
+		renderCards();
+}
 
 // #################################### EVENT LISTENERS ##############################################
 
-plus.addEventListener("click", function() {
-	form.style.visibility = "visible";
-});
-
-cancelButton.addEventListener("click", function() {
-	form.style.visibility = "hidden";
-	errorMessage_with_X.style.visibility = "hidden"; 
-})
-
-dustbin.addEventListener("click", () => {
-	bookList.length = 0;
-	localStorage.setItem("bookList", JSON.stringify(bookList));
-	renderCards();
-})
-
-theForm.addEventListener("submit", function(e) {
-	let flag = 1; 
-	
-	
-	if (+completedPages.value > +totalPages.value) {
-		flag = 0;
-		errorMessage_with_X.style.visibility = "visible"; 
-	}
-	if (bookAuthor.value.length >30) {
-		flag = 0;
-		errorMessage_with_X.style.visibility = "visible"; 
-	}
-	
-	
-	if (flag == 1){
-		// errorMessage_with_X.style.visibility = "hidden";
-		
-		// getting value from form and making an object to push in bookList
-		let book1 = book(bookName.value, bookAuthor.value, totalPages.value, completedPages.value); 	
-		bookList.push(book1);
-		localStorage.setItem("bookList", JSON.stringify(bookList));
-		
-		console.clear();
-		console.log(typeof bookList);
-		
-		// cardGenerator(bookName.value, bookAuthor.value, totalPages.value, completedPages.value);
-		renderCards();
-		updateInfo();
-	}
-	else {
-		e.preventDefault();
-	}
-	
-	
-});
-
-generateBtn.addEventListener("click", ()=> {
-
-		localStorage.clear();
-
-		bookList = [{
-		"name": "Organize your whiteboard", 
-		"author": "Narendra Bohora", 
-		"tPages": 99,
-		"cPages": 22
-	}, 
-	{
-		"name": "How To Control Your Anger", 
-		"author": "Balkrishna Subedi", 
-		"tPages": 127,
-		"cPages": 38
-	},
-	{
-		"name": "Control Your Students: The Science Of Silent Class", 
-		"author": "Navaraj Negi", 
-		"tPages": 118,
-		"cPages": 42
-	},
-	{
-		"name": "How to be productive teacher and skip the easy topics", 
-		"author": "Yogesh Raj Subedi", 
-		"tPages": 135,
-		"cPages": 46
-	},
-	{
-		"name": "The Subtle Art Of Acting", 
-		"author": "Anmol KC", 
-		"tPages": 69,
-		"cPages": 41
-	},
-	
-]
-		localStorage.setItem("bookList", JSON.stringify(bookList));
-		renderCards();
-})
+plus.addEventListener("click", () => { showForm() });
+cancelButton.addEventListener("click", ()=> { hideForm() });
+dustbin.addEventListener("click", () => { clearAndRender() });
+theForm.addEventListener("submit", function(e) { handleForm(e) });
+generateBtn.addEventListener("click", ()=> { populateLibrary() });
